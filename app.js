fc43eb35be2b95525e29babaa9cbb20e456e7def
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-06-15 19:40:33 +0200

    Refactoring code

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ function updateInput(e) {
   searchValue = e.target.value;
 }
 
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=15",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
-  );
-
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
   const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
@@ -39,27 +39,18 @@ async function curatedPhotos() {
   });
 }
 
+async function curatedPhotos() {
+  const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15");
+
+  generatePictures(data);
+}
+
 async function searchPhotos(query) {
-  const dataFetch = await fetch(
-    `https://api.pexels.com/v1/search?query=${query}&per_page=15`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15`
   );
 
-  const data = await dataFetch.json();
-  data.photos.forEach((photo) => {
-    const galleryImg = document.createElement("div");
-    galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-    <p>${photo.photographer}</p>
-    `;
-    gallery.appendChild(galleryImg);
-  });
+  generatePictures(data);
 }
 
 curatedPhotos();
